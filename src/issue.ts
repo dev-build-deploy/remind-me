162cbe1829cafb6e3e71d702cb8abc0c4950d2db
Kevin de Jong <monkaii@hotmail.com>
2023-07-13 14:21:56 +0200

    fixup! feat: extract TODOs and create barebones issues

--- src/issue.ts
+++ src/issue.ts
@@ -54,11 +54,10 @@ export function createIssue(file: string, body: string): string {
 
 export function isMatchingIssue(issue: Issue, file: string, title: string): boolean {
   return (
-    issue.labels.includes("TODO") &&
     issue.title === title &&
     issue.body !== null &&
     issue.body !== undefined &&
-    issue.body.includes(`@File: ${file}`)
+    issue.body.includes(`@File: ${file}\n`)
   );
 }
 
